764d790f6a34bac36145fb3bccf3d93845be40d4
pacificbelt30 <pacificbelt0201@outlook.jp>
2020-01-07 02:17:21 +0900

    コマンドをsakeに

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 mkdir ~/.cbuild
 mkdir ~/.cbuild/bin
-cp ./* ~/.cbuild/
+cp -r ./* ~/.cbuild/
 ln -s ~/.cbuild/cbuild.rb ~/.cbuild/bin/sake
 cp ~/.bashrc ~/.bashrc.bak
 echo "export CBPATH=~/.cbuild" >> ~/.bashrc
 echo "export PATH=\$PATH:\$CBPATH/bin" >> ~/.bashrc
 echo "cat \$CBPATH/vimascii" >> ~/.bashrc
 source ~/.bashrc
-rm $CBPATH/install.sh
+#rm $CBPATH/install.sh
